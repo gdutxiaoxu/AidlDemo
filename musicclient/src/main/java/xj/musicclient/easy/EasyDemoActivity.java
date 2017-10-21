0e47194d4238b1993a91b617fdefc909807abefd
xujun <xj2@meitu.com>
2017-10-21 21:04:05 +0800

    [NEW] 增加 EasyDemo 的例子

---
+++ musicclient/src/main/java/xj/musicclient/easy/EasyDemoActivity.java
@@ -0,0 +1,15 @@
+package xj.musicclient.easy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import xj.musicclient.R;
+
+public class EasyDemoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_easy_demo);
+    }
+}
